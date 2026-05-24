4d6a7c3fefa0527feed17b844477c30cb9bfc721
twodoot <tudorharabor@gmail.com>
2026-05-24 22:41:07 +0100

    did transmission code for the controller

--- src/ControllerCode.cpp
+++ src/ControllerCode.cpp
@@ -1,25 +1,36 @@
 #include <Arduino.h>
+#include <esp_now.h>
+#include <WiFi.h>
 
-void togglefunction();
-
+#define CHANNEL 1
 #define rollrate_max 360 // gyroscope rollrate maximum in deg/s
 #define rollangle_max 45 // accelerometer rollangle maximum in deg
 #define yawrate_max 360 // mximum yawrate in deg/s
 
 bool toggle_flight_mode = false;
-int lastswitch = HIGH;
-int curswitch;
+uint8_t lastswitch = HIGH;
+uint8_t curswitch;
+
+esp_now_peer_info_t slave;
 
 void setup() {
-  pinMode(D1,INPUT_PULLUP);
+  pinMode(D2,INPUT_PULLUP);
+
+  WiFi.mode(WIFI_STA);
+  esp_now_init();
+  uint8_t broadcastAddress[] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF}; //placeholder adress
+  memcpy(slave.peer_addr, broadcastAddress, 6);
+  slave.channel = CHANNEL;
+  slave.encrypt = false;
+  esp_now_add_peer(&slave);
 }
 
 void loop() {
-  short left_y = analogRead(A0);
-  short left_x = analogRead(A1);
+  uint16_t left_y = analogRead(A0);
+  uint16_t left_x = analogRead(A1);
 
-  short right_x = analogRead(A2);
-  short right_y = analogRead(A3);
+  uint16_t right_x = analogRead(A2);
+  uint16_t right_y = analogRead(A3);
 
   left_y = constrain(left_y, 520 ,1023);
   left_y = map(left_y, 520, 1023, 0, 180);
@@ -31,7 +42,7 @@ void loop() {
     delay(50);
     curswitch = digitalRead(D2);
     if (curswitch == LOW ) {
-      togglefunction();
+      toggle_flight_mode = !toggle_flight_mode;
     }
   }
   lastswitch = curswitch;
@@ -45,15 +56,9 @@ void loop() {
     right_y = map(right_y, 0, 1023, -rollangle_max, rollangle_max); // target angle in deg
     right_x = map(right_x, 0, 1023, -rollangle_max, rollangle_max); // target angle in deg
   }
-}
 
-void togglefunction() {
-    switch (toggle_flight_mode) {
-      case false:
-        toggle_flight_mode = true;
-        break;
-      case true:
-        toggle_flight_mode = false;
-        break;
-  }
+
+  uint16_t data[4] = {left_y, left_x, right_y, right_x};
+  esp_now_send(slave.peer_addr, (uint8_t *)data, sizeof(data));
+  
 }
