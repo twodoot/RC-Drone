7a17e2caef05b33ee8f8f7718fb16843addad147
twodoot <tudorharabor@gmail.com>
2026-06-21 23:39:49 +0100

    made getmacaddress funcitona easier to copy paste

--- src/GetMacAddress.cpp
+++ src/GetMacAddress.cpp
@@ -8,9 +8,10 @@ void setup() {
     WiFi.macAddress(addr);
     
     for (int i = 0; i <6; i++) {
+        Serial.print("0x");
         if (addr[i]< 0x10) {Serial.print("0");}
         Serial.print(addr[i], HEX);
-        if (i < 5) {Serial.print(":");}
+        if (i < 5) {Serial.print(", ");}
     }
 
 }
