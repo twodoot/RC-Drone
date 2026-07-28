b864da89a6acd23b3d8a1d188e8cf0fefbc03be4
twodoot <tudorharabor@gmail.com>
2026-07-28 17:59:49 +0100

    Mac Addresses code improved and build flags for it

--- src/GetMacAddress.cpp
+++ src/GetMacAddress.cpp
@@ -5,8 +5,9 @@ uint8_t addr[6];
 
 void setup() {
     Serial.begin(9600);
+    while(!Serial){delay(10);}
     WiFi.macAddress(addr);
-    
+    delay(1000);
     for (int i = 0; i <6; i++) {
         Serial.print("0x");
         if (addr[i]< 0x10) {Serial.print("0");}
@@ -17,5 +18,5 @@ void setup() {
 }
 
 void loop() {
-
+    
 }
